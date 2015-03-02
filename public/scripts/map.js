460013af25cdd567d7e1822015699fe3f26a0b9a
Add Nokia Here service and dependencies <spepple@spepple-MacBook-Pro.local>
2015-03-02 09:51:32 -0800

    Map styles and utils

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,17 +1,35 @@
 /* Do some uniteresting initialiation */
 var main_map = 'osaez.kp2ddba3';
-var side_map = 'osaez.lbh2oe11'
+var side_map = 'stevepepple.lbj8m1n3'
 L.mapbox.accessToken = 'pk.eyJ1Ijoib3NhZXoiLCJhIjoiOExKN0RWQSJ9.Hgewe_0r7gXoLCJHuupRfg';
 var map_options = {
-    attributionControl: false,
-    zoomControl: false,
-    maxZoom: 18,
-    minZoom: 5
+  attributionControl: false,
+  zoomControl: false,
+  maxZoom: 18,
+  minZoom: 5
+}
+
+var circle_outer = {
+  color:'#ACB63C',
+  opacity: 0,
+  weight: 1,
+  fillColor:'#ACB63C',
+  fillOpacity: .6
+}
+
+var circle_inner = {
+  color:'#8D9700',
+  opacity: 0,
+  weight: 1,
+  fillColor:'#8D9700',
+  fillOpacity: .6
 }
 
 // Setup the Leaflet Geocoder
 var geo_search;
 var city_name;
+var city_bounds;
+var timer;
 //initGeoCoder(showNeighborhood);
 
 function initGeoCoder(callback) {
@@ -24,24 +42,32 @@ function initGeoCoder(callback) {
   select_place.scrollWheelZoom.disable();
 
 	// Initialize the Leaflet Geocoder
-   city_name = $('option').not(function(){ return !this.selected }).text();
+   city_name = $('#cities option').not(function(){ return !this.selected }).text();
+
    geo_search = new L.Control.GeoSearch({
       provider: new L.GeoSearch.Provider.Google(),
+      showMarker: false,
       params : { city : city_name },
       callback : setResult
    }).addTo(select_place);
 
+   // Style the search area
+   $("#leaflet-control-geosearch-qry").addClass("topcoat-search-input--large")
+
+
    // See if there are new results
    var expire;
    function setResult(result) {
+       console.log("Geocode result: ", result)
       // Use a timer to reduce the number of map operations
       clearMap();
-      clearTimeout(expire);
+      //clearTimeout(expire);
       clearInterval(timer);
-      vehicles_query = {}
+      //vehicles_query = {}
 
       expire = setTimeout(function(){
          callback(result);
+         setNeighborhood(result, select_place)
          //showTransit(result)
       }, 1000);
    }
@@ -67,11 +93,13 @@ function showCityLayer(data, map) {
    // Show all hoods
    current_layer = L.geoJson(data, {
       style: {
-         cursor: "pointer", fillColor: '#00BAF4', fillOpacity: 0.05, weight: 1, opacity: 0.6, color: '#00BAF4'
+         cursor: "pointer", fillColor: '#00BAF4', fillOpacity: 0.1, weight: 2, opacity: 0.6, color: '#00BAF4'
       },
       onEachFeature: onEachFeature
    });
+
    current_layer.addTo(map);
+   city_bounds = current_layer.getBounds();
    map.fitBounds(current_layer.getBounds());
 
     function onEachFeature(feature, layer) {
@@ -79,7 +107,7 @@ function showCityLayer(data, map) {
      if (feature.properties && feature.properties.popupContent) {
           //layer.bindPopup(feature.properties.popupContent);
      }
-       layer.on({
+     layer.on({
           click: function(e) {
              var place = {}
              place.X = e.latlng.lng;
@@ -107,10 +135,35 @@ function showCityLayer(data, map) {
    if (current_layer !== null) { map.removeLayer(current_layer) }
 
    if(selected !== null) {
-
-     current_layer = L.geoJson(selected, {  fillColor: '#00BAF4', fillOpacity: 0.05, weight: 4, opacity: 0.4, color: '#00BAF4' })
+     current_layer = L.geoJson(selected, {  fillColor: '#BC2285', fillOpacity: 0.5, weight: 4, opacity: 0.6, color: '#9E005D' })
 	   current_layer.addTo(map);
+
+     // Setup the UI in cityUI
+     initAdd(place);
+
      //TODO: Should we zoom to it or just mark it as selected?
      //map.fitBounds(current_layer.getBounds());
+     //map.setZoom(map.getZoom() - 2)
    }
+}
+
+
+function centerPlaces() {
+  // Do some magic to make the maps fit
+  _.each(places, function(place){
+    var width = $("#" + place.place_id).width();
+    place.map._size.x = width;
+    //place.map.setView(place.marker)
+    place.map.panTo(place.marker)
+  });
+}
+
+// Util function to clear all features/markers
+function clearMap() {
+  select_place.removeLayer(current_layer)
+
+  for (i = 0; i < markers.length; i++) {
+    map.removeLayer(markers[i])
+  }
+
 }
