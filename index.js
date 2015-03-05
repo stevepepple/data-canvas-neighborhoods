6a236252398f7736bfb235a4b1a7cf545d6375bd
Add Nokia Here service and dependencies <spepple@spepple-mbp.push.mobileiron.com>
2015-03-04 18:29:12 -0800

    Parse the read me with node and make it the about page.

--- index.js
+++ index.js
@@ -4,8 +4,39 @@ var app = express();
 app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/public'));
 
+// Libraries for ReadMe
+var fs = require('fs');
+var marked = require('marked');
+
 app.get('/', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
   res.sendfile(__dirname + '/index.html');
+});
+
+
+app.get('/about', function(req, res){
+
+  function readModuleFile(path, callback) {
+	    try {
+	        var filename = require.resolve(path);
+	        fs.readFile(filename, 'utf8', callback);
+	    } catch (e) {
+	        callback(e);
+	    }
+	}
+
+  readModuleFile('./README.md', function (err, string) {
+	    console.log( "Converting Read Me" );
+		var header = 	'<meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>Urban Heartbeat</title><meta name="description" content=""><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">' +
+						'<link rel="stylesheet" href="../styles/main.css" type="text/css" media="screen"/>';
+
+		var html = "<html><head>" + header + "</head><body><header><img src='logo.svg'/></header><article class='markdown-body'>" + marked(string) + "</article></body>";
+    res.send( html );
+
+	});
+
 });
 
 app.listen(app.get('port'), function() {
