e611d423a718cf94e580c63fe19c40619a4eab0d
Add Nokia Here service and dependencies <spepple@spepple-MacBook-Pro.local>
2015-03-09 15:00:16 -0700

    Allow cross origin globally.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/public'));
+app.use(allowCrossDomain);
 
 // Libraries for ReadMe
 var fs = require('fs');
