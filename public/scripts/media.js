d2a20c50ac91c4fbeb897c521da4c74322daf793
stevepepple <spepple@spepple-MacBook-Pro.local>
2015-08-26 13:44:44 -0700

    Fiction for making strings title case.

--- public/scripts/media.js
+++ public/scripts/media.js
@@ -67,3 +67,13 @@ function showTime() {
   var now = moment();
   $(".time").html( now.format("hh:mm A") );
 }
+
+String.prototype.toTitleCase = function () {
+  var string = "";
+  var words = this.split(" ");
+  	for(keyvar in words) {
+  		string += ' ' + words[keyvar].substr(0,1).toUpperCase()
+    	+ words[keyvar].substr(1,words[keyvar].length);
+  	}
+  	return string;
+}
